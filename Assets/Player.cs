a24723da1b5ebeac369e47391d8c17e9dac52b63
ecv <ecv@ecv.ecv>
2021-09-01 05:20:50 +0200

    started character controller (finally a little fun)

---
+++ Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed=transform.localScale.x*20f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dT=Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.A))
+            transform.position=transform.position+Vector3.left*dT*speed;
+        if (Input.GetKey(KeyCode.RightArrow)||Input.GetKey(KeyCode.D))
+            transform.position=transform.position+Vector3.right*dT*speed;
+    }
+}
