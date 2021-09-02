bd52a90ab05bd77beb365aa57e7d7f28248595fa
ecv <ecv@ecv.ecv>
2021-09-02 03:22:12 +0200

    basic camera follow

---
+++ Assets/camFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camFollow : MonoBehaviour
+{
+    public Transform player;
+    float z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        z=transform.position.z;
+    }
+
+    void LateUpdate()
+    {
+        if (!player)
+            return;
+
+        transform.Translate(player.position-transform.position);
+        transform.position=new Vector3(transform.position.x, transform.position.y, z);
+
+    }
+}
